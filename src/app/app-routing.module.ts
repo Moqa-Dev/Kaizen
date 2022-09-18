17d7859c4a48fe58aa8dff641f73f892a6fda782
Shaheen <mahmoud_shaheen@live.com>
2022-09-18 15:58:23 +0200

    landing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full',
+  }, {
+    path: '',
+    children: [{
+      path: '',
+      loadChildren: () => import('./landing/landing.module').then(x => x.LandingModule)
+    }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,33 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { LandingComponent } from './landing.component';
 
-describe('AppComponent', () => {
+describe('LandingComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        LandingComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LandingComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'Kaizen'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('Kaizen');
-  // });
+  it(`should have as title 'Kaizen'`, () => {
+    const fixture = TestBed.createComponent(LandingComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Kaizen');
+  });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LandingComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('Kaizen app is running!');
